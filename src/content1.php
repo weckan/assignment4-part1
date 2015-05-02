342fa16fe67be898817781733d18a65c67625b73
Andrew Weckwerth <weckwera@onid.oregonstate.edu>
2015-05-02 08:55:32 -0500

    Moved header modifier above first output

--- src/content1.php
+++ src/content1.php
@@ -2,6 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 session_start();
+if(isset($_GET['logout'])) {
+   if($_GET['logout'] == 'true') {
+    $_SESSION = array();
+    session_destroy();
+    $filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+    $filePath = implode('/',$filePath);
+    $redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
+    header("Location: {$redirect}/login.php", true);
+    die();
+   }
+}
 ?>
 <!DOCTYPE html>
 <html>
